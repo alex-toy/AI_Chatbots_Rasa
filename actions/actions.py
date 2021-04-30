b0f98dd0533ee3e982841faf7ac519bc94f97a8d
alessio rea <alexei.80@hotmail.fr>
2021-04-30 09:25:19 +0200

    terminology slots

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,29 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionConfirm(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_confirm"
+
+    def run(self, 
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]
+        ) -> List[Dict[Text, Any]]:
+
+        service = tracker.get_slots('service')
+        p_count = tracker.get_slots('p_count')
+        print(service, p_count)
+
+        message_to_user = f"Hi there!! You booked {p_count} {service} tickets. Please confirm your booking."
+
+        dispatcher.utter_message(text=message_to_user)
+
+        return []
+
+
+
