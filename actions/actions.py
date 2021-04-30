be717b15fea8b236245eef52bf648736a4c5dfb9
alessio rea <alexei.80@hotmail.fr>
2021-04-30 10:47:39 +0200

    editing actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,29 +1,33 @@
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+from rasa_sdk.forms import FormAction
 
 
-class ActionConfirm(Action):
 
+class ActionPizzaOrderForm(FormAction):
+    
     def name(self) -> Text:
-        return "action_confirm"
+        return "action_pizza_order_form"
 
-    def run(self, 
-            dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]
-        ) -> List[Dict[Text, Any]]:
 
-        service = tracker.get_slots('service')
-        p_count = tracker.get_slots('p_count')
-        print(service, p_count)
+    @staticmethod
+    def required_slots(tracker: "Tracker") -> List[Text]:
+        return ["pizza_size", "pizza_type", "pizza_amount"]
 
-        message_to_user = f"Hi there!! You booked {p_count} {service} tickets. Please confirm your booking."
 
-        dispatcher.utter_message(text=message_to_user)
+    def submit(self,
+               dispatcher: "CollectingDispatcher",
+               tracker: "Tracker",
+               domain: "DomainDict",
+        ) -> List[Dict]:
+        
+        pizza_size = tracker.get_slot('pizza_size')
+        pizza_type = tracker.get_slot('pizza_type')
+        pizza_amount = tracker.get_slot('pizza_amount')
+        print(pizza_amount, pizza_size, pizza_type)
 
+        dispatcher.utter_message(text=f"ok Great. Your order is {pizza_amount} {pizza_type} pizza in {pizza_size} size. Can you please confirm your order?")
         return []
-
-
 
