9ec6cb18547a8de3428d0ee201677f0b63141b21
alessio rea <alexei.80@hotmail.fr>
2021-04-30 11:42:31 +0200

    init web site conversation flow

--- actions/actions.py
+++ actions/actions.py
@@ -36,3 +36,27 @@ class ActionPizzaOrderForm(FormAction):
 
 
 
+class ActionWebSiteOrderForm(FormAction):
+    
+    def name(self) -> Text:
+        return "action_web_site_order_form"
+
+
+    @staticmethod
+    def required_slots(tracker: "Tracker") -> List[Text]:
+        return ["web_site_type"]
+
+
+    def submit(self,
+               dispatcher: "CollectingDispatcher",
+               tracker: "Tracker",
+               domain: "DomainDict",
+        ) -> List[Dict]:
+        
+        web_site_type = tracker.get_slot('web_site_type')
+        print(web_site_type)
+
+        message_to_user = f"ok Great. Your order is a {web_site_type} web site. Can you please confirm your order?"
+
+        dispatcher.utter_message(text=message_to_user)
+        return []
